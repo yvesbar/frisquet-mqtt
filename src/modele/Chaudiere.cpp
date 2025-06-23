40ce2cbf92ee77060f10ba9e5a24c7cd1003ce14
yvesbar <108618795+yvesbar@users.noreply.github.com>
2025-06-23 08:53:49 +0200

    Evite mle 0 de 11h

--- src/modele/Chaudiere.cpp
+++ src/modele/Chaudiere.cpp
@@ -40,25 +40,22 @@ Zone* Chaudiere::getZoneById(byte id) {
     if (zones.find(id) == zones.end()) {
         DEBUG(F("Chaudiere - Création d'une nouvelle zone avec l'ID : "));
         DEBUGLN(id);
-        
+        // TODO ???
     }
     return zones[id]; // Retourne la zone existante ou nouvellement créée
 }
 
 void Chaudiere::setTempExterieure(float value) {
-    if (value != tempExterieure) {
+    //Evite la trame de 11h qui set la température extérieure à 0.0 (origine inconnue)
+    if (value != 0.0 || abs(value - tempExterieure) < 0.5) {
         tempExterieure = value;
     }
 }
 
 void Chaudiere::setTempCorpsDeChauffe(float value) {
-    if (value != tempCorpsDeChauffe) {
-        tempCorpsDeChauffe = value;
-    }
+    tempCorpsDeChauffe = value;
 }
 
 void Chaudiere::setTempECS(float value) {
-    if (value != tempECS) {
-        tempECS = value;
-    }
+    tempECS = value;
 }
